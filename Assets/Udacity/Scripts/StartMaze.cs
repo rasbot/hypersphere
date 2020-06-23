91cbfdf700e9f0dc281f6d6f0123aafbad9e36a1
rasbot <nathan.f.rasmussen@gmail.com>
2020-06-22 19:53:54 -0700

    add game files

---
+++ Assets/Udacity/Scripts/StartMaze.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMaze : MonoBehaviour {
+
+    public float DestroyTime;
+
+
+	// Use this for initialization
+	void Start () {
+                   
+        Destroy(gameObject, DestroyTime);
+
+	}
+	
+}
